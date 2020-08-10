fa8c3087390f64f1388ac05b9368ddbd1a487923
maskadev <sidkiyouness@gmail.com>
2020-08-10 15:05:05 +0100

    offline mode fix

--- lib/loading.dart
+++ lib/loading.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
 import 'package:wallpaper_code_source/home.dart';
+import 'dart:io';
 
 class Loading extends StatefulWidget {
   @override
@@ -11,25 +13,40 @@ class _LoadingState extends State<Loading> {
   List animewallpaper = [];
 
   Future<void> getData() async {
-    var result = await Firestore.instance
-        .collection("wallpaper")
-        .getDocuments()
-        .then((QuerySnapshot snapshot) => snapshot.documents.forEach((f) {
-              animewallpaper = f.data.values.toList();
-              print(animewallpaper);
-              Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => Home(
-                            animewallpaper: animewallpaper,
-                          )));
-            }));
+    try {
+      var result = await Firestore.instance
+          .collection("wallpaper")
+          .getDocuments()
+          .then((QuerySnapshot snapshot) => snapshot.documents.forEach((f) {
+                animewallpaper = f.data.values.toList();
+                print(animewallpaper);
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Home(
+                              animewallpaper: animewallpaper,
+                            )));
+              }));
+    } on PlatformException {}
+  }
+
+  checkinternit() async {
+    try {
+      final result = await InternetAddress.lookup('google.com');
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        print('connected');
+      }
+    } on SocketException catch (_) {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => Error()));
+    }
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    checkinternit();
     getData();
   }
 
@@ -45,6 +62,33 @@ class _LoadingState extends State<Loading> {
               ),
             ],
           )),
+    );
+  }
+}
+
+class Error extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.02,
+              ),
+              Text(
+                "please check your internet connection and try again",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
